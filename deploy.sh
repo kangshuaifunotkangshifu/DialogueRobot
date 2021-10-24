980d284f34142f6e6609a20aea25c12c329e001e
dengbowen <dengbowen17@outlook.com>
2021-10-25 00:48:01 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+port=8080
+pid = $(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{print $1}')
+if [-n "$pid"]; then
+    kill -9 $pid
+fi
+
 source /usr/local/anaconda3/etc/profile.d/conda.sh
 conda activate robot
 git -C ~/DialogueRobot pull
