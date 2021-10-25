fd72a03d3ae5a7839517d29288e4c7b66aa0bc9d
dengbowen <dengbowen17@outlook.com>
2021-10-25 11:45:55 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-port=8080
-pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{print $1}')
-if [ -n "$pid" ]; then
-    kill -9 $pid;
-fi
-
-source /usr/local/anaconda3/etc/profile.d/conda.sh
-conda activate robot
-git -C ~/DialogueRobot pull
-python ~/DialogueRobot/DiaRobot/manage.py runserver 0.0.0.0:8080
+tmux kill-session -t django
+tmux new-session -d -s django
+tmux send-keys "cd ~/DialogueRobot" C-m
+tmux send-keys "git pull" C-m
+tmux send-keys "cd ~/DialogueRobot/DiaRobot" C-m
+tmux send-keys "conda activate robot" C-m
+tmux send-keys "python manage.py runserver 0.0.0.0:8080" C-m
