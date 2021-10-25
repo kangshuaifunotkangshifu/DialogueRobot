368ce1d3eb381951f09270b0fd0bbba8f00db7d8
zhangkang <1446540925@qq.com>
2021-10-25 13:14:34 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ tmux send-keys "cd ~/DialogueRobot" C-m
 tmux send-keys "git pull" C-m
 tmux send-keys "cd ~/DialogueRobot/DiaRobot" C-m
 tmux send-keys "conda activate robot" C-m
-tmux send-keys "python manage.py runserver 0.0.0.0:8080" C-m
+tmux send-keys "python manage.py runserver 0.0.0.0:8000" C-m
