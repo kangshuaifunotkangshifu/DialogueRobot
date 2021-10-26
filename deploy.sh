94ef6c471f8929052bc860d725d9beb4b3184c42
dengbowen <dengbowen17@outlook.com>
2021-10-26 22:19:01 +0800

    deploy by tmux

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-# tmux kill-session -t django
-# tmux new-session -d -s django
-# tmux send-keys "cd ~/DialogueRobot" C-m
-# tmux send-keys "git pull" C-m
-# tmux send-keys "cd ~/DialogueRobot/DiaRobot" C-m
-# tmux send-keys "conda activate robot" C-m
-# tmux send-keys "python manage.py runserver 0.0.0.0:8000" C-m
-port=8000
-pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{print $1}')
-if [ -n "$pid" ]; then
-    kill -9 $pid;
-fi
+tmux kill-session -t django
+tmux new-session -d -s django
+tmux send-keys "cd ~/DialogueRobot" C-m
+tmux send-keys "git pull" C-m
+tmux send-keys "cd ~/DialogueRobot/DiaRobot" C-m
+tmux send-keys "conda activate robot" C-m
+tmux send-keys "python manage.py runserver 0.0.0.0:8000" C-m
 
-source /usr/local/anaconda3/etc/profile.d/conda.sh
-conda activate robot
-git -C ~/DialogueRobot pull
-nohup python ~/DialogueRobot/DiaRobot/manage.py runserver 0.0.0.0:8000 >/dev/null 2>&1 &
+# port=8000
+# pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{print $1}')
+# if [ -n "$pid" ]; then
+#     kill -9 $pid;
+# fi
+
+# source /usr/local/anaconda3/etc/profile.d/conda.sh
+# conda activate robot
+# git -C ~/DialogueRobot pull
+# nohup python ~/DialogueRobot/DiaRobot/manage.py runserver 0.0.0.0:8000 >/dev/null 2>&1 &
