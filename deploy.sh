1dced4ac39684a819899eacd49b1e326edcf4953
dengbowen <dengbowen17@outlook.com>
2021-10-28 16:05:23 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,7 @@ tmux new-session -d -s django
 tmux send-keys "cd ~/DialogueRobot" C-m
 tmux send-keys "git pull" C-m
 tmux send-keys "cd ~/DialogueRobot/DiaRobot" C-m
-tmux send-keys "conda activate robot" C-m
-tmux send-keys "sudo python manage.py runserver 0.0.0.0:80" C-m
+tmux send-keys "sudo ~/.conda/envs/robot/bin/python manage.py runserver 0.0.0.0:80" C-m
 
 # port=8000
 # pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{print $1}')
